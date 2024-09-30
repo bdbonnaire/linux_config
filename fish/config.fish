4566ccd75a2dc2132a47d2187d5db0508ce14bef
Basile D. Bonnaire <bdbonnaires@gmail.com>
2024-09-30 10:26:20 +0200

    update

--- fish/config.fish
+++ fish/config.fish
@@ -13,3 +13,18 @@ function fish_user_key_bindings
     fish_vi_key_bindings --no-erase insert
 end
 starship init fish | source
+zoxide init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/bdb/miniforge3/bin/conda
+    eval /home/bdb/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/bdb/miniforge3/etc/fish/conf.d/conda.fish"
+        . "/home/bdb/miniforge3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/bdb/miniforge3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
